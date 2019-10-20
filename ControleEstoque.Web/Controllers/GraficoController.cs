1d586a30950a8781e7816c557ac2080e574ffa89
JVasconcelosSilva <jefferson.vasconcelos1@outlook.com.br>
2019-10-20 19:31:23 -0300

    autenticação, sessão, cookies, login e logoff

--- ControleEstoque.Web/Controllers/GraficoController.cs
+++ ControleEstoque.Web/Controllers/GraficoController.cs
@@ -9,10 +9,12 @@ namespace ControleEstoque.Web.Controllers
     public class GraficoController : Controller
     {
         // GET: Grafico
+        [Authorize]
         public ActionResult PerdasMes()
         {
             return View();
         }
+        [Authorize]
         public ActionResult EntradaSaidaMes()
         {
             return View();
